4957fe546509715e93df332327514371d6adbf77
Caio Colaiacovo Carneiro da Costa <caiohshot@gmail.com>
2019-10-03 23:06:07 -0400

    Atualizando componente de diálogo

--- src/componentes/Dialogo.js
+++ src/componentes/Dialogo.js
@@ -4,32 +4,28 @@ class Dialogo extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      deveExibir: props.deveExibir
-    };
+    document.body.style.overflowY = 'hidden';
   }
-
+  
   render() {
     return (
-      <div class="dialogo dialogo_medio">
-        <div class="dialogo__container">
-          <div class="dialogo__cabecalho">
-            <h2 class="dialogo__titulo">Titulo do diálogo</h2>
-            <p class="dialogo__subtitulo">Descrição do título</p>
+      <div className={`dialogo dialogo_medio ${this.props.deveExibir ? "dialogo_ativo" : ""}`}>
+        <div className="dialogo__container">
+          <div className="dialogo__cabecalho">
+            <h2 className="dialogo__titulo">Titulo do diálogo</h2>
+            <p className="dialogo__subtitulo">Descrição do título</p>
           </div>
 
-          <div class="dialogo__corpo">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam feugiat sagittis diam. Duis sed mi vel erat commodo euismod. 
-            Nunc blandit blandit purus rhoncus tempor. Donec tincidunt tempus massa, sed vulputate eros semper eget. 
-            Duis sit amet lorem ac nisl bibendum sodales. Etiam efficitur feugiat elit dictum efficitur. 
-            Donec eget lectus pharetra, viverra neque id, laoreet felis. Nunc finibus ante non nisl sodales congue.
+          <div className="dialogo__corpo">
+            Componente do diálogo para React, seguindo os padrões do DigixUI
           </div>
 
-          <div class="dialogo__rodape">
-            Teste
+          <div className="dialogo__rodape">
+            <button className="botao botao_cor-info" type="button">Salvar</button>
+            <button className="botao botao_cor-cinza botao_contorno" onClick={this.props.fecharModal} type="submit">Cancelar</button>
           </div>
-          <button class="dialogo__botao-fechar" type="button" onclick="fecharDialogo()" aria-label="Fechar diálogo">
-            <i class="fa fa-times" aria-label="hidden"></i>
+          <button className="dialogo__botao-fechar" type="button" aria-label="Fechar diálogo" onClick={this.props.fecharModal}>
+            <i className="fa fa-times" aria-label="hidden"></i>
           </button>
         </div>
       </div>
