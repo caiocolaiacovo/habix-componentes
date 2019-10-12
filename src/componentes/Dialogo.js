527f94e1303df822766bbc72f306b76b6744ebf8
Caio Colaiacovo Carneiro da Costa <caiohshot@gmail.com>
2019-10-11 23:38:41 -0400

    Melhorias no componente de diálogo

--- src/componentes/Dialogo.js
+++ src/componentes/Dialogo.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
 class Dialogo extends Component {
-  constructor(props) {
-    super(props);
+  componentDidUpdate(prevProps) {
+    if (!prevProps.deveSerExibido && this.props.deveSerExibido) {
+      this.setState({ultimoElementoComFoco: document.activeElement});
+      return;
+    }
+
+    if (prevProps.deveSerExibido && !this.props.deveSerExibido) {
+      this.state.ultimoElementoComFoco.focus();
+    }
   }
 
   componentWillUpdate(nextProps) {
-    console.log('document.body.style.overflowY: ', document.body.style.overflowY);
-
     if (nextProps.deveSerExibido)
       document.body.style.overflowY = 'hidden';
     else
@@ -15,26 +20,32 @@ class Dialogo extends Component {
   }
 
   render() {
+    const { corpo: Corpo, rodape: Rodape } = this.props;
+    
     return (
       <div className={`dialogo dialogo_medio ${this.props.deveSerExibido ? "dialogo_ativo" : ""}`}>
         <div className="dialogo__container">
+
+          {/* todosElementosDoDialogo[1] */}
           <div className="dialogo__cabecalho">
             <h2 className="dialogo__titulo">Titulo do diálogo</h2>
             <p className="dialogo__subtitulo">Descrição do título</p>
           </div>
 
           <div className="dialogo__corpo">
-            Componente do diálogo para React, seguindo os padrões do DigixUI
+            <Corpo></Corpo>
           </div>
 
           <div className="dialogo__rodape">
-            <button className="botao botao_cor-info" type="button" onClick={this.props.acaoPrincipal}>Salvar</button>
-            <button className="botao botao_cor-cinza botao_contorno" type="submit" onClick={this.props.fecharDialogo}>Cancelar</button>
+            <Rodape></Rodape>
           </div>
+
           <button className="dialogo__botao-fechar" type="button" aria-label="Fechar diálogo" onClick={this.props.fecharDialogo}>
             <i className="fa fa-times" aria-label="hidden"></i>
           </button>
         </div>
+        {/* WTF?? Remover ao fechar */}
+        <span tabindex="0"></span>
       </div>
     );
   }
